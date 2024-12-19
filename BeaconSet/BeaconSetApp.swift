c4117d906f32efd08a681eb122903015cff68216
서충원 <cwseo@vestellalab.com>
2024-12-19 17:44:13 +0900

    Initial Commit

---
+++ BeaconSet/BeaconSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconSetApp.swift
+//  BeaconSet
+//
+//  Created by 베스텔라랩 on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BeaconSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeaconSet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeaconSet
+//
+//  Created by 베스텔라랩 on 12/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
