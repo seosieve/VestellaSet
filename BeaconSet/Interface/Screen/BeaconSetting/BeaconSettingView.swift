3f3a708c167cef59244ede6aa902411cfa2df45a
SEOSIEVE <royalcircle97@naver.com>
2025-01-20 23:55:35 +0900

    FEAT: BeaconSettingView Config

---
+++ BeaconSet/Interface/Screen/BeaconSetting/BeaconSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  BeaconSettingView.swift
+//  BeaconSet
+//
+//  Created by 서충원 on 1/20/25.
+//
+
+import SwiftUI
+
+struct BeaconSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BeaconSettingView()
+}
