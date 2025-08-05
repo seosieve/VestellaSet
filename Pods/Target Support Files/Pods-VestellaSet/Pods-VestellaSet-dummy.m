6a4ac6a4da60a2d3d260123722ac8af48711c51b
SEOSIEVE <royalcircle97@naver.com>
2025-08-05 20:27:49 +0900

    CHORE: Naming 변경 Cross Path Resolve

---
+++ Pods/Target Support Files/Pods-VestellaSet/Pods-VestellaSet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VestellaSet : NSObject
+@end
+@implementation PodsDummy_Pods_VestellaSet
+@end
--- Pods/Target Support Files/Pods-VestellaSet/Pods-VestellaSet-umbrella.h
+++ Pods/Target Support Files/Pods-VestellaSet/Pods-VestellaSet-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_BeaconSetVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BeaconSetVersionString[];
+FOUNDATION_EXPORT double Pods_VestellaSetVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_VestellaSetVersionString[];
 
