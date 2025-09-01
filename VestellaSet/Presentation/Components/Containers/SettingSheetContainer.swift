41b962eb9dad15ab4284b3a2ac5b482d4a69a1e5
SEOSIEVE <royalcircle97@naver.com>
2025-09-01 13:19:26 +0900

    FIX: SettingContainer Dismiss시 뷰 밀림 해결

--- VestellaSet/Presentation/Components/Containers/SettingSheetContainer.swift
+++ VestellaSet/Presentation/Components/Containers/SettingSheetContainer.swift
@@ -12,6 +12,7 @@ struct SettingSheetContainer: View {
     let store: StoreOf<BeaconSettingFeature>
     
     @State private var offsetY: CGFloat = 0
+    @State private var opacity: CGFloat = 0
     
     var body: some View {
         ZStack {
@@ -29,16 +30,26 @@ struct SettingSheetContainer: View {
                 .frame(maxWidth: .infinity)
                 .glassSettingSheetStyle()
                 .offset(y: offsetY)
+                .opacity(opacity)
                 .padding(.horizontal, Spacing.s20)
             }
         }
         .onChange(of: store.isSheetPresented) { _, newValue in
             if newValue {
-                withAnimation(.bouncy) { offsetY = 0 }
+                withAnimation(.bouncy) {
+                    offsetY = 0
+                    opacity = 1
+                }
             } else {
-                withAnimation(.easeInOut(duration: 0.2)) { offsetY = store.sheetType.height + 120 }
+                withAnimation(.easeInOut(duration: 0.2)) {
+                    offsetY = store.sheetType.height + 120
+                    opacity = 0
+                }
             }
         }
-        .onAppear { offsetY = store.sheetType.height + 120 }
+        .onAppear {
+            offsetY = store.sheetType.height + 120
+            opacity = 0
+        }
     }
 }
