e1c7fc9fd6ff553c1544810e9e61b8191615fa19
SEOSIEVE <royalcircle97@naver.com>
2025-09-23 13:57:51 +0900

    FEAT: Keyboard Avoidance 동작 제거

--- VestellaSet/Presentation/Features/BeaconSetting/BeaconSettingView.swift
+++ VestellaSet/Presentation/Features/BeaconSetting/BeaconSettingView.swift
@@ -14,24 +14,28 @@ struct BeaconSettingView: View {
     @FocusState private var isFocused: Bool
     
     var body: some View {
-        ZStack {
-            VStack(spacing: Spacing.s12) {
-                BackButton { store.send(.clickBackButton) }
-                SettingTitleLabel()
-                GlassTextField(store: store, isFocused: $isFocused)
-                GlassSelectionButton(type: .interval(store.interval)) { isFocused = false; store.send(.showSheet(.interval))}
-                GlassSelectionButton(type: .power(store.power)) { isFocused = false; store.send(.showSheet(.power))}
-                ActionButton(type: .save) { store.send(.clickSaveButton) }
-                Spacer()
+        GeometryReader { geometry in
+            ZStack {
+                VStack(spacing: Spacing.s12) {
+                    BackButton { store.send(.clickBackButton) }
+                    SettingTitleLabel()
+                    GlassTextField(store: store, isFocused: $isFocused)
+                    GlassSelectionButton(type: .interval(store.interval)) { isFocused = false; store.send(.showSheet(.interval))}
+                    GlassSelectionButton(type: .power(store.power)) { isFocused = false; store.send(.showSheet(.power))}
+                    ActionButton(type: .save) { store.send(.clickSaveButton) }
+                    Spacer()
+                }
+                .blur(radius: store.isSheetPresented ? Radius.s8 : Radius.zero)
+                .animation(.easeOut, value: store.isSheetPresented)
+                
+                SettingSheetContainer(store: store)
             }
-            .blur(radius: store.isSheetPresented ? Radius.s8 : Radius.zero)
-            .animation(.easeOut, value: store.isSheetPresented)
-            SettingSheetContainer(store: store)
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .monoBackground()
+            .navigationBarBackButtonHidden()
+            .onAppear { store.send(.onAppear) }
+            .onTapGesture { isFocused = false }
         }
-        .monoBackground()
         .ignoresSafeArea(.keyboard)
-        .navigationBarBackButtonHidden()
-        .onAppear { store.send(.onAppear) }
-        .onTapGesture { isFocused = false }
     }
 }
